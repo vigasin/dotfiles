a299e44fbef02d9c908fc8e0f19fd64c89f72153
Ivan Vigasin <ivigasin@gmail.com>
2024-04-21 22:25:45 -0700

    Add .hcl file type

--- .config/nvim/lua/josean/plugins/lspconfig.lua
+++ .config/nvim/lua/josean/plugins/lspconfig.lua
@@ -12,7 +12,7 @@ return {
 
 		lspconfig.terraformls.setup({})
 		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-			pattern = { "*.tf", "*.tfvars" },
+			pattern = { "*.tf", "*.tfvars", "*.hcl" },
 			callback = function()
 				vim.lsp.buf.format()
 			end,
