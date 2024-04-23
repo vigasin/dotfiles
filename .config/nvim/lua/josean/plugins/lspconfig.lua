2035d7dbd1d9c98ca6cd497dac231bc08e8b652c
Ivan Vigasin <ivigasin@gmail.com>
2024-04-22 19:40:12 -0700

    Add few languages to the treesitter configuration

--- .config/nvim/lua/josean/plugins/lspconfig.lua
+++ .config/nvim/lua/josean/plugins/lspconfig.lua
@@ -12,7 +12,7 @@ return {
 
 		lspconfig.terraformls.setup({})
 		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-			pattern = { "*.tf", "*.tfvars", "*.hcl" },
+			pattern = { "*.tf", "*.tfvars" },
 			callback = function()
 				vim.lsp.buf.format()
 			end,
--- .config/nvim/lua/josean/plugins/treesitter.lua
+++ .config/nvim/lua/josean/plugins/treesitter.lua
@@ -1,57 +1,66 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"css",
+				"dockerfile",
+				"gitignore",
+				"go",
+				"graphql",
+				"hcl",
+				"helm",
+				"html",
+				"javascript",
+				"jq",
+				"json",
+				"ledger",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"prisma",
+				"proto",
+				"query",
+				"sql",
+				"svelte",
+				"terraform",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
